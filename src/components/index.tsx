597ef3a8ded01230e146a722cf6e349ff335a0df
Qodestackr <winchygichu@gmail.com>
2023-07-25 17:16:47 +0300

    fix: Correct barrel import for Header component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,5 @@
 export { default as Navbar } from "./navbar/Navbar";
 export { default as Footer } from "./footer/Footer";
-export { Header } from "./header/Header";
+export { default as Header } from "./header/Header";
 export { default as Contact } from "./contact/Contact";
 export { default as Skills } from "./skills/Skills";
