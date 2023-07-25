82f81345a077070bc32457bad51108770826063f
Qodestackr <winchygichu@gmail.com>
2023-07-25 17:31:46 +0300

    Add Signup/Login pages

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,15 +1,17 @@
 import Home from "../pages/home/Home";
 import ErrorPage from "../pages/error-page/ErrorPage";
-import UnderConstruction from "../pages/constuction/UnderConstruction";
+// import UnderConstruction from "../pages/constuction/UnderConstruction";
 import UserMap from "../pages/web-map/UserMap";
 import Profile from "../pages/profile/Profile";
+import Login from "../pages/login/Login";
+import SignUp from "../pages/signup/SignUp";
 
 export const routes = [
   { path: "/", element: <Home /> },
   { path: "/profile", element: <Profile /> },
   { path: "/locateMe", element: <UserMap/> },
-  { path: "/login", element: <UnderConstruction /> },
-  {path: '/signup', element: <UnderConstruction /> },
+  { path: "/login", element: <Login /> },
+  {path: '/signup', element: <SignUp /> },
   { path: "/logout", element: <Home /> },
   { path: "*", element: <ErrorPage /> }, // Catch-all undefined routes
 ];
