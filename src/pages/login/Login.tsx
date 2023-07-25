6f04bbc2027de025a4b3992217f28665ee0fa9bc
Qodestackr <winchygichu@gmail.com>
2023-07-25 17:45:17 +0300

    pages/Login button color

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -44,7 +44,7 @@ function Login() {
         <div className="flex justify-center">
           <button
             onClick={handleLogin}
-            className="px-6 py-3 rounded-md bg-indigo-600 text-white font-semibold hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            className="px-6 py-3 rounded-md bg-green-600 text-white font-semibold hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500"
           >
             Login
           </button>
