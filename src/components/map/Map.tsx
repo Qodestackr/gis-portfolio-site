3fa8ee7b243aca8182b77479668fa6421ae8868c
Qodestackr <winchygichu@gmail.com>
2023-07-26 07:17:31 +0300

    Fix: Vercel/Project typings

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,21 +1,28 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
- 
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+} from "react-leaflet";
+
+
+type LatLngTuple = [number, number];
+
 function Map() {
-  const position = [-1.2921, 36.8219];
+  const position: LatLngTuple = [-1.2921, 36.8219];
   const zoomLevel = 15;
 
   return (
     <MapContainer zoom={zoomLevel} center={position} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
       <Marker position={position}>
-        <Popup>
-          User Info! {/**TODO: ADD */}
-        </Popup>
+        <Popup>User Info! {/**TODO: ADD */}</Popup>
       </Marker>
     </MapContainer>
   );
 }
 
-export default Map
+export default Map;
