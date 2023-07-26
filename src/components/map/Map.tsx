59d9f84cfe6b55fa701a8c705f6c2e608d2743bf
Qodestackr <winchygichu@gmail.com>
2023-07-26 08:00:02 +0300

    fix position typo

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,21 +1,20 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
- 
+
 function Map() {
-  const position = [-1.2921, 36.8219];
+  const position = { lat: -1.2921, lng: 36.8219 };
   const zoomLevel = 15;
 
   return (
     <MapContainer zoom={zoomLevel} center={position} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
       <Marker position={position}>
-        <Popup>
-          User Info! {/**TODO: ADD */}
-        </Popup>
+        <Popup>User Info! {/**TODO: ADD */}</Popup>
       </Marker>
     </MapContainer>
   );
 }
 
-export default Map
+export default Map;
